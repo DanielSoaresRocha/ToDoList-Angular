1278c5cd420a25c87ee39bfd6147247a9378d1d4
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-13 17:03:11 -0300

    adicionando um directiva

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { FormsModule } from '@angular/forms'
 
-import { TarefaService } from './shared';
+import {
+  TarefaService,
+  TarefaConcluidaDirective
+} from './shared';
 import { ListarTarefasComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
-import { EditarTarefasComponent } from './editar'
+import { EditarTarefasComponent } from './editar';
 
 @NgModule({
   declarations: [
     ListarTarefasComponent,
     CadastrarTarefaComponent,
-    EditarTarefasComponent
+    EditarTarefasComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule,
